6a133ede694047d01dfa91a9c2e53ccf85c82a51
Jordon Perkins <jordon.rubel@gmail.com>
2022-12-07 10:00:48 -0600

    Application Views in good shape

--- src/components/views/ApplicationViews.js
+++ src/components/views/ApplicationViews.js
@@ -1,6 +1,24 @@
+import { WishListHome } from "../wishListHome/WishListHome.js"
+import { Outlet, Route, Routes } from "react-router-dom"
+
 export const ApplicationViews = () => {
 	return <>
+	
+        <Routes>
+
+            <Route path="/" element={
+                <>
+                    <h1>WishList</h1>
+                    <div>Create your own WishList Today</div>
+
+                    <Outlet />
+                </>
+            }></Route>
+            
+            <Route path="wishListHome" element={ <WishListHome />  } />
+
+        </Routes>
+
 
 	</>
 }
-

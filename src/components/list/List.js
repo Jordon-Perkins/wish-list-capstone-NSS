8f55515e6d4b516f4d26bf2000133d9722a9915d
Jordon Perkins <jordon.rubel@gmail.com>
2022-12-07 10:01:50 -0600

    Added some notes for List.js and ListItemForm.js

---
+++ src/components/list/List.js
@@ -0,0 +1,5 @@
+// Locations.js kandyKorner
+
+//new item form button will send you to Listitemform.js
+
+//will have terunary to show which lists are shown
---
+++ src/components/list/ListItemForm.js
@@ -0,0 +1,7 @@
+// productForm.js kandyKorner
+
+//Customerform/employeefrom honeyRaes
+
+//save new item button will send you back list.js
+
+//you can only edit your own list
